276cacc4931e7848890c674538e3a68634ca6bf3
sarah <alessandrofeitoza01@gmail.com>
2023-02-28 09:59:23 -0300

    exemplo JSON

--- aula21/eventos.js
+++ aula21/eventos.js
@@ -39,14 +39,42 @@ let contatos = [
     {
         nome: "Marilia",
         telefone: "88 9 7845-1234"
+    },
+    {
+        nome: "Maria Chiquinha",
+        telefone: "88 9 7845-1234"
+    },
+    {
+        nome: "Ana Maria",
+        telefone: "88 9 7845-1234"
+    },
+    {
+        nome: "fulana",
+        telefone: "85 12361"
     }
 ];
 
 
-
-for (x = 0; x <= 9; x++) {
-    lista.innerHTML += `<li> <strong> ${contatos[x].nome}: </strong> ${contatos[x].telefone} </li>`;
+function exibir(cadaContato) {
+    lista.innerHTML += `<li> <strong> ${cadaContato.nome}: </strong> ${cadaContato.telefone} </li>`;
 }
+
+contatos.forEach(exibir);
+
+
+
+// contatos.forEach(function (cadaContato) {
+//     lista.innerHTML += `<li> <strong> ${cadaContato.nome}: </strong> ${cadaContato.telefone} </li>`;
+// });
+
+// contatos.forEach((cadaContato) => {
+//     lista.innerHTML += `<li> <strong> ${cadaContato.nome}: </strong> ${cadaContato.telefone} </li>`;
+// });
+
+
+// for (x = 0; x <= 9; x++) {
+//     lista.innerHTML += `<li> <strong> ${contatos[x].nome}: </strong> ${contatos[x].telefone} </li>`;
+// }
 
 
 // let nome = "Lane";
