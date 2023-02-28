eec603af463ae432ed53728b81e4994cb0d7f553
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-02-28 08:00:45 -0300

    foreach tabela

--- alunos/js/darkmode.js
+++ alunos/js/darkmode.js
@@ -38,3 +38,73 @@ function ativarDarkmode() {
 
     alterarCores();
 }
+
+
+/////////////////// ARRAY DE OBJETOS ///////////////////////
+
+
+let alunos = [
+    {
+        id: 1,
+        nome: "Ana kelly",
+        email: "kelly@gmail.com",
+        telefone: "00 00000-0000",
+        cidade: "Fortaleza-ce"
+    },
+    {
+        id: 2,
+        nome: "Andy Silva",
+        email: "andy@gmail.com",
+        telefone: "00 00000-0000",
+        cidade: "Fortaleza-ce"
+    },
+    {
+        id: 3,
+        nome: "Samantha kuran",
+        email: "samantha@gmail.com",
+        telefone: "00 00000-0000",
+        cidade: "Fortaleza-ce"
+    },
+    {
+        id: 4,
+        nome: "Lane Gomes",
+        email: "lane@gmail.com",
+        telefone: "00 00000-0000",
+        cidade: "Fortaleza-ce"
+    },
+    {
+        id: 5,
+        nome: "Sarah Camilo",
+        email: "kelly@gmail.com",
+        telefone: "00 00000-0000",
+        cidade: "Fortaleza-ce"
+    }
+];
+
+function listarAlunos(){
+    let tabela = document.getElementById("tabela-body");
+    alunos.forEach((aluno) => {
+        tabela.innerHTML += `<tr>
+                                <td>${aluno.id}</td>
+                                <td>${aluno.nome}</td>
+                                <td>${aluno.email}</td>
+                                <td>${aluno.telefone}</td>
+                                <td>${aluno.cidade}</td>
+                                <td>
+                                    <button class="btn btn-warning">Editar</button>
+                                    <button class="btn btn-danger">Excluir</button>
+                                </td>
+                            </tr>`;
+    })
+}
+
+function salvarAluno(){
+    event.preventDefault();
+
+    let aluno = {
+        nome: nome.value,
+        email: email.value,
+        telefone: telefone.value,
+        cidade: cidade.value
+    } 
+    alunos.push(aluno) }
