31520caeb0e28d17cd6e6352355a86200259d8ac
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-03-07 11:50:51 -0300

    atualizado

--- alunos/js/alunoController.js
+++ alunos/js/alunoController.js
@@ -1,8 +1,6 @@
 //READ
 function listarAlunos(){
-
     // let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
-
     fetch("http://localhost:8000/alunos")
     .then(res => res.json())
     .then(alunos => {
@@ -10,14 +8,14 @@ function listarAlunos(){
         tabela.innerHTML = "";
         alunos.forEach((aluno, key) => {
             tabela.innerHTML += `<tr>
-                                    <td>${key}</td>
+                                    <td>${aluno.id}</td>
                                     <td>${aluno.nome}</td>
                                     <td>${aluno.email}</td>
                                     <td>${aluno.telefone}</td>
                                     <td>${aluno.cidade}</td>
                                     <td>
                                         <button class="btn btn-warning">Editar</button>
-                                        <button class="btn btn-danger" onClick="deletarAluno(${key})">Excluir</button>
+                                        <button class="btn btn-danger" onClick="deletarAluno(${aluno.id})">Excluir</button>
                                     </td>
                                 </tr>`;
         })
@@ -29,37 +27,56 @@ function listarAlunos(){
 //CREATE
 function salvarAluno(){
     event.preventDefault();
-    let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+    //let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
     let aluno = {
         nome: nome.value,
         email: email.value,
         telefone: telefone.value,
         cidade: cidade.value
     } 
-    alunos.push(aluno);
-    localStorage.setItem('alunos', JSON.stringify(alunos));
-    alert("Salvo com Sucesso!");
-
+    fetch("http://localhost:8000/alunos", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(aluno) 
+    })
+    .then(res => res.json())
+    .then(resposta => {
+        alert("Salvo com Sucesso!");
+        console.log(resposta)
+    })
+    // alunos.push(aluno);
+    // localStorage.setItem('alunos', JSON.stringify(alunos));
 }
 
 //UPDATE
 function atualizarAluno(){}
 
+
 //DELETE
 function deletarAluno(id){
-    let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
-    let alunosAtualizado = [];
+    // let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+    // let alunosAtualizado = [];
 
-    alunos.forEach((aluno, key) => {
-        if(id != key){
-            alunosAtualizado.push(aluno);
-        }
-    });
+    // alunos.forEach((aluno, key) => {
+    //     if(id != key){
+    //         alunosAtualizado.push(aluno);
+    //     }
+    // });
 
-    localStorage.setItem('alunos', JSON.stringify(alunosAtualizado));
+    // localStorage.setItem('alunos', JSON.stringify(alunosAtualizado));
 
-    alert("Excluido com sucesso!");
+    fetch(`http://localhost:8000/alunos/${id}`, {
+        method:"DELETE",
+    })
+    .then(res => res.json())
+    .then(resposta => {
+        alert("Excluido com sucesso!");
+        console.log(resposta)
+        listarAlunos();
+    })
 
-    listarAlunos();
+
 }
 
