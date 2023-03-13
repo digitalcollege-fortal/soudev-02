9c4ba78fde2b9a04e888b16d97c61c76a68b444d
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-03-13 10:00:25 -0300

    Add material-ui

--- projeto-final/front/src/components/Navbar/index.js
+++ projeto-final/front/src/components/Navbar/index.js
@@ -1,7 +1,28 @@
+import {Button, TextField, Alert, Badge} from "@mui/material";
+import {Favorite, Group, ShoppingCart} from "@mui/icons-material";
+
+import "./styles.css";
+
 export default function Navbar() {
     return (
         <div>
+            <Favorite/>
+            <Group/>
+
+            <Badge color="primary" badgeContent={"4"}>
+                <ShoppingCart className="red"/>
+            </Badge>
+
             Navbar
+
+            <Alert severity="error">Aqui vai a mensagem</Alert>
+
+            <button>Entrar</button>
+
+            <Button color="secondary" variant="contained">Entrar</Button>
+
+            <TextField label="O que vc procura"/>
+
         </div>
     )
 }
