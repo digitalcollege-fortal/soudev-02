93a5463f182be0bc02dfb29455a0fd0ddfc421b3
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-03-13 09:20:23 -0300

    menu

--- projeto-final/front/src/components/Menu/index.js
+++ projeto-final/front/src/components/Menu/index.js
@@ -1,14 +1,24 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 import "./styles.css";
 
 export default function Menu() {
+    const location = useLocation();
+
+    const isActive = (url) => {
+        if (location.pathname === url) {
+            return "active";
+        }
+
+        return "";
+    };
+
     return (
         <div className="menu">
-            <Link to="/">Home</Link>
-            <Link to="/produtos">Produtos</Link>
-            <Link to="/categorias">Categorias</Link>
-            <Link to="/meus-pedidos">Meus Pedidos</Link>
+            <Link to="/" className={isActive('/')}>Home</Link>
+            <Link to="/produtos" className={isActive('/produtos')}>Produtos</Link>
+            <Link to="/categorias" className={isActive('/categorias')}>Categorias</Link>
+            <Link to="/meus-pedidos" className={isActive('/meus-pedidos')}>Meus Pedidos</Link>
         </div>
     )
 }
