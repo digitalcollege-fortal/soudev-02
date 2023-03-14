442c4a08226b8c5ca9ac22e251b0a2ab8b4e21a6
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-03-14 09:56:15 -0300

    Aula de useState

--- projeto-final/front/src/pages/Produtos/index.js
+++ projeto-final/front/src/pages/Produtos/index.js
@@ -1,34 +1,64 @@
 import Menu from "../../components/Menu";
-import {Card, Button} from "@mui/material";
+import {Card, Button, Grid, Badge} from "@mui/material";
 import React from "react";
-// import {useState} from "react";
 
-
-export default function Produtos() {
-    // let quantidade = 10;
+function CadaProduto(props) {
     const [quantidade, alterarQuantidade] = React.useState(1);
 
     const add = () => {
-        quantidade += 1;
+        alterarQuantidade(quantidade + 1);
     }
 
     const remove = () => {
-        quantidade -=1 ;
+        alterarQuantidade(quantidade - 1);
     }
 
+    return (
+        <Card style={{width: '100%', padding: '10px'}}>
+            {props.produto} ( R$ {props.valor} )
+
+            <Button disabled={ quantidade === 1 ? true : false }  onClick={remove}>-</Button>
+            {quantidade}
+            <Button onClick={add}>+</Button>
+
+            <hr/>
+
+            TOTAL: R$ {props.valor * quantidade }
+
+            <br/>
+            {
+                props.desconto && 
+                <Badge color="primary" badgeContent={props.desconto}/>
+            }
+        </Card>
+    )
+}
+
+
+export default function Produtos() {
     return (
         <div>
             <Menu/>
             Pagina de Produtos
             <hr/>
 
-            <Card>
-                <Button onClick={remove}>-</Button>
-                {quantidade}
-                <Button onClick={add}>+</Button>
-            </Card>
+            <Grid container spacing={4}>
+                <Grid item>
+                    <CadaProduto produto="Pratinho" valor="8.90"/>
+                </Grid>
 
-            
+                <Grid item>
+                    <CadaProduto produto="Heineken" valor="10" desconto="10%"/>
+                </Grid>
+
+                <Grid item>
+                    <CadaProduto produto="Coca Cola" valor="4.90"/>
+                </Grid>
+
+                <Grid item>
+                    <CadaProduto produto="Agua com gas" valor="3.90"/>
+                </Grid>
+            </Grid>
         </div>
     )
 }
