8288ae4a2f274d9fc351184e0d22a1af40e9f778
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-03-14 07:57:09 -0300

    parte do lourão

--- projeto-final/front/src/components/Produto/index.jsx
+++ projeto-final/front/src/components/Produto/index.jsx
@@ -1,4 +1,5 @@
-import { Paper, Stack } from '@mui/material';
+import { Button, Paper, Stack } from '@mui/material';
+import { blue, pink } from '@mui/material/colors';
 import React from 'react';
 import sapato from './produto.png';
 
@@ -7,11 +8,12 @@ export default function Produto()
     return(
         <>
             <Stack>
-                <Paper elevation={2}>
+                <Paper elevation={2} sx={{backgroundColor: pink['A200']}}>
                     <img src={sapato} />
                 </Paper>
                 <span>Categoria</span>
                 <h4>Nome do produto</h4>
+                <Button sx={{ backgroundColor: blue[500]}}>Compre agora</Button>
             </Stack>
         </>
     );
