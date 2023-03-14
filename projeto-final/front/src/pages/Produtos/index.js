21946e1511be68b7de4fbd9c7cc42ee4e9b5bcee
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-03-14 08:49:08 -0300

    useState

--- projeto-final/front/src/pages/Produtos/index.js
+++ projeto-final/front/src/pages/Produtos/index.js
@@ -1,7 +1,34 @@
+import Menu from "../../components/Menu";
+import {Card, Button} from "@mui/material";
+import React from "react";
+// import {useState} from "react";
+
+
 export default function Produtos() {
+    // let quantidade = 10;
+    const [quantidade, alterarQuantidade] = React.useState(1);
+
+    const add = () => {
+        quantidade += 1;
+    }
+
+    const remove = () => {
+        quantidade -=1 ;
+    }
+
     return (
         <div>
+            <Menu/>
             Pagina de Produtos
+            <hr/>
+
+            <Card>
+                <Button onClick={remove}>-</Button>
+                {quantidade}
+                <Button onClick={add}>+</Button>
+            </Card>
+
+            
         </div>
     )
 }
