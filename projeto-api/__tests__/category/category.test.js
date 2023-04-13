6d6b9b59f9d519c325ffcf28f8cbc58e2094ece5
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2023-04-13 11:57:00 -0300

    update

--- projeto-api/__tests__/category/category.test.js
+++ projeto-api/__tests__/category/category.test.js
@@ -6,11 +6,32 @@ test('testando endpoint buscarTodos', async () => {
     expect(typeof JSON.parse(resultado)).toBe('object');
 })
 
-test('testando endpoint listarUm', async () => {
+test('testando controller listarUm', async () => {
     let resultado = JSON.parse(await controller.listOne(1));
     expect(typeof resultado).toBe('object');
     expect(typeof resultado.id).toBe('number');
     expect(typeof resultado.nome).toBe('string');
     expect(typeof resultado.status).toBe('number');
     expect(typeof resultado.created_at).toBe('string');
+})
+
+// test('testando a rota listarUm', async () => {
+//     let resultado = JSON.parse(await routes.get('/categories/a'));
+//     expect(resultado).toBe("Unknown column 'a' in 'where clause'")
+// })
+
+test('testando se esta inserindo na tabela', async () => {
+    let dados = {
+        nome: 'teste',
+        status: 1,
+    }
+    let resultado = JSON.parse(await controller.create(JSON.stringify(dados)));
+    expect(typeof resultado).toBe('object');
+    expect(typeof resultado.id).toBe('number');
+})
+
+test('testando se esta deletando', async () => {
+    await controller.destroy(1);
+    let resultado = JSON.parse(await controller.listOne(1));
+    expect(resultado.status).toBe(2);
 })
